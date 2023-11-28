c408b6810ea1ce020a28ff019bf87c1f388f15f7
Tracy Zhao <tracyzhao24@g.ucla.edu>
2023-11-27 23:33:15 -0800

    fix centering on page and body background color

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 100%;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
