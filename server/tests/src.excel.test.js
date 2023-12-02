14fe425da30d7dededa28b400a629bb23368ae50
Victor Wen <vicwen02@g.ucla.edu>
2023-12-02 00:11:46 -0800

    Test case for ExcelTable read

--- server/tests/src.excel.test.js
+++ server/tests/src.excel.test.js
@@ -9,7 +9,7 @@ test('Add data to table', async () => {
         data: [
             {Student: "Joe Bruin", School: "UCLA", Unknown: "N/A"},
             {School: "UCLA", Student: "Tracy Zhao"},
-            ["Barack Obama", "Harvard"]
+            {Student: "Barack Obama", School: "Harvard"}
         ]
     });
 
@@ -31,4 +31,45 @@ test('Add data to table', async () => {
         [,"Tracy Zhao", "UCLA"],
         [,"Barack Obama", "Harvard"]
     ]);
+});
+
+test('ExcelTable is invertible', async () => {
+    const testTable = new ExcelTable();
+    testTable.add_data("Schools attended", {
+        headers: ["Student", "School"], 
+        data: [
+            {Student: "Joe Bruin", School: "UCLA", Unknown: "N/A"},
+            {School: "UCLA", Student: "Tracy Zhao"},
+            {Student: "Barack Obama", School: "Harvard"}
+        ]
+    });
+    testTable.add_data("School Rivalries", {
+        headers: ["School A", "School B"], 
+        data: [
+            {"School A": "UCLA", "School B": "USC"},
+            {"School A": "Harvard", "School B": "Yale"}
+        ]
+    });
+
+    const pass = new stream.PassThrough();
+    await testTable.write(pass);
+
+    const newTable = new ExcelTable();
+    await newTable.read(pass);
+
+    const sheet_names = newTable.get_sheet_names();
+    expect(sheet_names).toEqual(["Schools attended", "School Rivalries"]);
+
+    const sheet1 = newTable.get_data("Schools attended");
+    expect(sheet1.headers).toEqual(["Student", "School"]);
+    expect(sheet1.data.length).toBe(3);
+    expect(sheet1.data).toContainEqual({Student: "Joe Bruin", School: "UCLA"});
+    expect(sheet1.data).toContainEqual({Student: "Tracy Zhao", School: "UCLA"});
+    expect(sheet1.data).toContainEqual({Student: "Barack Obama", School: "Harvard"});
+
+    const sheet2 = newTable.get_data("School Rivalries");
+    expect(sheet2.headers).toEqual(["School A", "School B"]);
+    expect(sheet2.data.length).toBe(2);
+    expect(sheet2.data).toContainEqual({"School A": "UCLA", "School B": "USC"});
+    expect(sheet2.data).toContainEqual({"School A": "Harvard", "School B": "Yale"});
 });
