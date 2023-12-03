38ff2e76c4fd797d62fdbe9d0b546565b645dfa1
Tracy Zhao <tracyzhao24@g.ucla.edu>
2023-12-02 18:05:07 -0800

    api call for download file

--- client/src/containers/DownloadExcelFile.js
+++ client/src/containers/DownloadExcelFile.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Navigate } from "react-router-dom";
+import axios from 'axios';
+import * as XLSX from 'xlsx';
+
 import styled from 'styled-components'
 import PageHeader from '../components/PageHeader'
 import MappingList from '../components/MappingList';
@@ -92,45 +95,48 @@ const DownloadExcelFile = () => {
 
     // TODO: send mappings to endpoint and get Excel file from endpoint
     const handleSubmit = async (e) => {
-        console.log(e)
+        console.log(checked)
     
         // Send selected mappings list to the server
-        try {
-            const response = await fetch('/api/submit-form', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(checked),
-            });
-            // await MappingService.createMapping(mappingData);
+        await axios.post('/tables/download', checked)
+            .then(response => response.json())
+            .then(data => {
+                // Convert data to worksheet
+                const ws = XLSX.utils.json_to_sheet(data);
+
+                // Create a workbook with a single sheet
+                // TODO: need to make multiple sheets
+                const wb = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+                // Create a Blob from the workbook
+                const fileBlob = XLSX.write(wb, { bookType: 'xlsx', type: 'blob' });
+
+                // Create a blob URL for the file
+                const fileUrl = URL.createObjectURL(fileBlob);
+
+                // Create an anchor element to trigger download
+                const downloadLink = document.createElement('a');
+                downloadLink.href = fileUrl;
+                downloadLink.download = 'data.xls'; // Set the file name
+
+                // Append the anchor to the body and trigger the download
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+
+                // Remove the anchor element from the body
+                document.body.removeChild(downloadLink);
+                URL.revokeObjectURL(fileUrl);
+            
+
             setModal((prev) => ({
                 ...prev,
                 show: true,
                 success: true,
                 successText: 'Successfully created Excel file to download',
             }))
-    
-            // Assuming the server responds with a file to download
-            const fileBlob = await response.blob();
-
-            // Create a blob URL for the file
-            const fileUrl = URL.createObjectURL(fileBlob);
-
-            // Create an anchor element to trigger download
-            const downloadLink = document.createElement('a');
-            downloadLink.href = fileUrl;
-            downloadLink.download = 'data.xls'; // Set the file name
-
-            // Append the anchor to the body and trigger the download
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-
-            // Remove the anchor element from the body
-            document.body.removeChild(downloadLink);
-            URL.revokeObjectURL(fileUrl);
-        } catch (error) {
-            console.error('Error submitting selected mappings:', error);
+        })
+        .catch (error => {
             console.log(error);
             setModal((prev) => ({
                 ...prev,
@@ -138,7 +144,7 @@ const DownloadExcelFile = () => {
                 success: false,
                 errorText: 'An error occurred trying to fetch the data',
             }))
-        }
+        });
     };
 
     if (state.user === null) {
@@ -156,7 +162,7 @@ const DownloadExcelFile = () => {
                             <div key={index}>
                                 <input 
                                     key={item.uuid} 
-                                    value={item.write_query} 
+                                    value={item.read_query} 
                                     type="checkbox" 
                                     onChange={handleCheck} />
                                 <span> {item.read_query} </span>
@@ -165,7 +171,7 @@ const DownloadExcelFile = () => {
                     </ListContainer>
                 </form>
 
-                <SubmitButton type="submit">
+                <SubmitButton type="submit" onClick={handleSubmit}>
                     Submit
                 </SubmitButton>
             </Form>
