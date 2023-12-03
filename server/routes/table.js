11e3bdd338202505d9a8d85d3b180a1eb426463a
richieej <78634556+richieej@users.noreply.github.com>
2023-12-03 11:17:22 -0800

    added error handling for table and user API

--- server/routes/table.js
+++ server/routes/table.js
@@ -2,6 +2,7 @@ const express = require("express");
 const stream = require('stream');
 const ExcelJS = require('exceljs');
 const fs = require('fs');
+const os = require('os');
 const multer = require('multer')
 const upload = multer({ dest: os.tmpdir() })
 
@@ -14,23 +15,33 @@ const applier = new MappingApplier();
 
 //receive a mapping, return a excel table (in the form of a stream)
 tableRoutes.route("/tables/download").post(async (req, res) => {
-    let mappings = req.body.mappings;
-    let table = await applier.table_from_mapping(mappings);
-    const file = await table.writeBuffer();
+    try {
+        let mappings = req.body.mappings;
+        let table = await applier.table_from_mapping(mappings);
+        const file = await table.writeBuffer();
 
-    res.send(file);
+        res.send(file);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 
 });
 
 //receive a table, commit mapping
 tableRoutes.route("/tables/upload", upload.single('file')).post(async (req, res) => {
-    let table = new ExcelTable();
-    const mappings = Object.values(req.body.data);
-    await table.readBuffer(req.file.buffer);
+    try {
+        let table = new ExcelTable();
+        const mappings = Object.values(req.body.data);
+        await table.readBuffer(req.file.buffer);
 
-    const write_result = await applier.update_from_table(table, mappings);
-    res.json(write_result);
-    
+        const write_result = await applier.update_from_table(table, mappings);
+        res.json(write_result);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
 })
 
 module.exports = tableRoutes;
