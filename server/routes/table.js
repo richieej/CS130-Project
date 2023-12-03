8230809a748c5caf18f11200d042f7fc035131ea
richieej <78634556+richieej@users.noreply.github.com>
2023-12-02 19:46:48 -0800

    fixed endpoints with frontend

--- server/routes/table.js
+++ server/routes/table.js
@@ -2,6 +2,8 @@ const express = require("express");
 const stream = require('stream');
 const ExcelJS = require('exceljs');
 const fs = require('fs');
+const multer = require('multer')
+const upload = multer({ dest: os.tmpdir() })
 
 const { MappingApplier } = require("../src/mapping_applier");
 const { ExcelTable } = require('../src/excel.js');
@@ -10,38 +12,25 @@ const { ExcelTable } = require('../src/excel.js');
 const tableRoutes = express.Router();
 const applier = new MappingApplier();
 
-
-//const dbo = require("../db/conn");
-
 //receive a mapping, return a excel table (in the form of a stream)
 tableRoutes.route("/tables/download").post(async (req, res) => {
     let mappings = req.body.mappings;
-    let resp, err = await applier.table_from_mapping(mappings);
-    if (resp) {
-        if (err) {
-            console.log(err);
-        }
-        res.sendStatus(200) // success
-    } else {
-        if (err) {
-            console.log(err);
-        }
-        res.sendStatus(400); // failed
-    }
-    
+    let table = await applier.table_from_mapping(mappings);
+    const file = await table.writeBuffer();
+
+    res.send(file);
+
 });
 
 //receive a table, commit mapping
-tableRoutes.route("/tables/upload").post(async (req, res) => {
+tableRoutes.route("/tables/upload", upload.single('file')).post(async (req, res) => {
     let table = new ExcelTable();
-    const dropdownPairs = req.body.data;
-    const mappings = Object.values(dropdownPairs);
-    const stream = req.body.stream;
-    await table.read(stream);
+    const mappings = Object.values(req.body.data);
+    await table.readBuffer(req.file.buffer);
 
     const write_result = await applier.update_from_table(table, mappings);
     res.json(write_result);
-  
+    
 })
 
 module.exports = tableRoutes;
